b743009335937e9f9ca4178d3bbbc0f4a695770a
Jamie Myland <jamie.myland@gmail.com>
2020-10-29 09:58:13 +0000

    Created instances of Robot and Human

--- Humanoids/Program.cs
+++ Humanoids/Program.cs
@@ -6,7 +6,25 @@ namespace Humanoids
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            Human human1 = new Human();
+            human1.Name = "Kai";
+
+            Robot robot1 = new Robot();
+            robot1.Designation = "OO-P0";
+
+            Console.WriteLine(human1.GetDetails());
+            human1.Speak("Hello");
+            human1.Walk();
+            human1.Eat();
+            human1.Sleep();
+
+            Console.WriteLine(robot1.GetDetails());
+            robot1.Speak("Hello");
+            robot1.Walk();
+            robot1.Recharge();
+            robot1.FireLaser();
+
         }
     }
 }
